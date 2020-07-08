ea7cc2bc900674b84d37442b652e463125acdf84
Vladlena Panchenko <panchenko.vladlena@gmail.com>
2020-07-08 19:54:19 +0200

    Burger menu closes on outside click

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-var btnMenuBurger = document.getElementsByClassName("burger")[0];
+var btnMenuBurger = document.getElementsByClassName("burger__icon")[0];
 var menuBurger = document.getElementsByClassName("burger__items")[0];
+var menuIcon = document.getElementsByClassName("burger")[0];
 
 btnMenuBurger.addEventListener(
   "click",
-  function () {
+  function (event) {
     menuBurger.classList.toggle("current");
-    btnMenuBurger.classList.toggle("closed");
+    menuIcon.classList.toggle("closed");
+    event.stopPropagation();
   },
   false
 );
+
+window.addEventListener("click", function () {
+  if (menuBurger.classList.contains("current")) {
+    menuBurger.classList.remove("current");
+    menuIcon.classList.remove("closed");
+  }
+});
